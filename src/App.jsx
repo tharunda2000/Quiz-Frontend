36eeffe0f428fbdc9b4b690cb3d8eb276640adf2
Tharunda De Silva <tharundadesilva@gmail.com>
2025-07-24 19:29:46 +0530

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react'
 import Context from './context/Context'
 import HomePage from './pages/HomePage'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const App = () => {
+
+  const router = createBrowserRouter([
+
+    {
+      path:'/',
+      element:<HomePage/>
+    }
+
+  ]);
   
   return (
     <div>
 
-      <HomePage/>
+      <RouterProvider router={router}/>
 
       
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,11 +10,16 @@ const {logedIn,setLogedIn} =useAppContext();
 
   return (
     <div className='flex fixed bg-blue-200 w-screen h-25 justify-between items-center'>
-        <img src={logo} alt="" srcset="" className='w-20 ms-15' />
-        <div className='flex'>
+        <img src={logo} className='w-20 ms-15' />
+        <div className='flex gap-20 text-2xl'>
             
+
         </div>
-        <input type="button" value={logedIn?"Log Out":"Login"} className='me-10'/>
+
+        <div>
+            <input type="button" value={"Login"} className={logedIn ?'hidden me-10':'me-10 '}/>
+            <input type="button" value={"Logout"} className={!logedIn ?'hidden me-10':'me-10 '}/>
+        </div>
       
     </div>
   )
